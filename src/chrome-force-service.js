778451d5861b8e20e654a1772078900c43d67f9f
Diéffrei Tiepo de Quadros <dieffrei.quadros@topinformation.com.br>
2016-12-12 20:05:38 -0200

    released helper methods

--- src/chrome-force-service.js
+++ src/chrome-force-service.js
@@ -16,6 +16,11 @@ angular.module('chromeForce', [])
             return url.indexOf('visual.force.com') >= 0;
         }
 
+        function isOnSalesforceDomain(url) {
+            if (url == null) return false;
+            return isLightning(url) || isStandardUi(url) || isVisualforce(url);
+        }
+
         function getInstanceName(url) {
             if (isStandardUi(url) || isLightning(url)) {
                 return url.split("//")[1].split(/.salesforce/)[0];
@@ -26,11 +31,10 @@ angular.module('chromeForce', [])
 
         return {
 
-            isOnSalesforceDomain : function (url) {
+            getIsOnSalesforceDomain : function (url) {
                 var deferred = $q.defer();
                 this.getCurrentUrl().then(function (url) {
-                    if (url == null) deferred.resolve(false);
-                    deferred.resolve(isLightning(url) || isStandardUi(url) || isVisualforce(url));
+                    deferred.resolve(isOnSalesforceDomain(url));
                 });
                 return deferred.promise;
             },
